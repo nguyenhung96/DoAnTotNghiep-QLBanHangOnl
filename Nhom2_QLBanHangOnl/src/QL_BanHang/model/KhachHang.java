9a936e5193a1aa9bdab51fd8cfed6e3ab90687f1
TriPhan2501 <49446197+JohnyPhan@users.noreply.github.com>
2020-11-12 12:53:59 +0700

    Tri commit model

---
+++ Nhom2_QLBanHangOnl/src/QL_BanHang/model/KhachHang.java
@@ -0,0 +1,97 @@
+package QL_BanHang.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "KhachHang")
+public class KhachHang implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3221326695082255954L;
+		@Id
+		@Column(name = "MaKH")
+		private String MaKH;
+		
+		@Column(name = "HoTenKH")
+		private String HoTenKH;
+		
+		@Column(name = "NgaySinh")
+		private Date NgaySinh;
+		
+		@Column(name = "MatKhau")
+		private String MatKhau;
+		
+		@Column(name = "SDT")
+		private String SDT;
+		
+		@Column(name = "Email")
+		private String Email;
+		
+		@Column(name = "DiaChi")
+		private String DiaChi;
+
+		public String getMaKH() {
+			return MaKH;
+		}
+
+		public void setMaKH(String maKH) {
+			MaKH = maKH;
+		}
+
+		public String getHoTenKH() {
+			return HoTenKH;
+		}
+
+		public void setHoTenKH(String hoTenKH) {
+			HoTenKH = hoTenKH;
+		}
+
+		public Date getNgaySinh() {
+			return NgaySinh;
+		}
+
+		public void setNgaySinh(Date ngaySinh) {
+			NgaySinh = ngaySinh;
+		}
+
+		public String getMatKhau() {
+			return MatKhau;
+		}
+
+		public void setMatKhau(String matKhau) {
+			MatKhau = matKhau;
+		}
+
+		public String getSDT() {
+			return SDT;
+		}
+
+		public void setSDT(String sDT) {
+			SDT = sDT;
+		}
+
+		public String getEmail() {
+			return Email;
+		}
+
+		public void setEmail(String email) {
+			Email = email;
+		}
+
+		public String getDiaChi() {
+			return DiaChi;
+		}
+
+		public void setDiaChi(String diaChi) {
+			DiaChi = diaChi;
+		}
+		
+}
